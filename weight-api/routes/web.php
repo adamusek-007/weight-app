24ec9c5669cf384a459a042fda408e372abe8c9d
adamusek-007 <adam.kuczabinski@gmail.com>
2025-06-30 19:42:17 +0200

    Add Route for weights POST

--- weight-api/routes/web.php
+++ weight-api/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\WeightController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/weights', [WeightController::class, 'store']);
