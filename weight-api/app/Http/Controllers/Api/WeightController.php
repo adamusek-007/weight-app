1d3626e3bdd9d5da1db9f2a6cab5caae4eefec7c
adamusek-007 <adam.kuczabinski@gmail.com>
2025-06-30 19:42:51 +0200

    Create API Controller for Weights

--- weight-api/app/Http/Controllers/Api/WeightController.php
+++ weight-api/app/Http/Controllers/Api/WeightController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Weight;
 use Illuminate\Http\Request;
+
 
 class WeightController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'value' => 'required|numeric'
+        ]);
+
+        $weight = new Weight();
+        $weight->value = $validated['value'];
+        $weight->save();
+
+        return response()->json([
+            'message' => 'Waga zapisana pomyślnie!',
+            'data' => $weight
+        ], 201);
+    }
 }
