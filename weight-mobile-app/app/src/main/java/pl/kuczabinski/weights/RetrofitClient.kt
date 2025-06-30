8da64e47b5de68f2eebbed9e1646824d95429133
adamusek-007 <adam.kuczabinski@gmail.com>
2025-06-30 21:51:31 +0200

    Correct URL

--- weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/RetrofitClient.kt
+++ weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://192.168.100.71:8000/weights")
+        .baseUrl("http://192.168.100.71:8000/weights/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
