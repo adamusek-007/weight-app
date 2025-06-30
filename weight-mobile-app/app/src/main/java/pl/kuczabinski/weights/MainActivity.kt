e3bbc1b50d0d091f7207b5c821acbd9806fa5558
adamusek-007 <adam.kuczabinski@gmail.com>
2025-06-30 21:44:59 +0200

    Make MainActivity.kt

--- weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/MainActivity.kt
+++ weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/MainActivity.kt
@@ -1,47 +1,50 @@
 package pl.kuczabinski.weights
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import pl.kuczabinski.weights.ui.theme.WeightsTheme
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var editTextWeight: EditText
+    private lateinit var buttonSend: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            WeightsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        editTextWeight = findViewById(R.id.editTextWeight)
+        buttonSend = findViewById(R.id.buttonSend)
+
+        buttonSend.setOnClickListener {
+            val weightValue = editTextWeight.text.toString().toFloatOrNull()
+            if (weightValue != null) {
+                val weight = Weight(weightValue)
+                RetrofitClient.apiService.sendWeight(weight)
+                    .enqueue(object : Callback<ResponseBody> {
+                        override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                            if (response.isSuccessful) {
+                                Toast.makeText(this@MainActivity, "Wysłano!", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this@MainActivity, "Błąd serwera!", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                            Toast.makeText(this@MainActivity, "Błąd sieci!", Toast.LENGTH_SHORT).show()
+                        }
+                    })
+            } else {
+                Toast.makeText(this, "Podaj poprawną wagę", Toast.LENGTH_SHORT).show()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeightsTheme {
-        Greeting("Android")
     }
 }
