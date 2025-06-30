51a1c9db31c5057db6cb854eaefcea3dffe589d3
adamusek-007 <adam.kuczabinski@gmail.com>
2025-06-30 21:45:24 +0200

    Make RetroFitClient object

---
+++ weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/RetrofitClient.kt
@@ -0,0 +1,13 @@
+package pl.kuczabinski.weights
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://192.168.100.71:8000/weights")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
+}
