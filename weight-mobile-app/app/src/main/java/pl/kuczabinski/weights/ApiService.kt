8291d1ccdde32cb273a2f1e8916c627d6cddc400
adamusek-007 <adam.kuczabinski@gmail.com>
2025-07-12 12:56:58 +0200

    Create API endpoints for mobile App

--- weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/ApiService.kt
+++ weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/ApiService.kt
@@ -3,9 +3,24 @@ package pl.kuczabinski.weights
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("weights")
-    fun sendWeight(@Body weight: Weight): Call<ResponseBody>
+    fun sendWeight(
+        @Body weight: Weight
+    ): Call<ResponseBody>
+
+    @POST("register")
+    fun postRegister(
+//        @Header("Authorization") authToken: String,
+        @Body user: User
+    ): Call<ResponseBody>
+
+    @POST("login")
+    fun postLogin(
+//        @Header("Authorization") authToken: String,
+        @Body user: User
+    ): Call<ResponseBody>
 }
