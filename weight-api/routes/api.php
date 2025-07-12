4a6da8ebdb745815d5fa146802963af7e6e29c9b
adamusek-007 <adam.kuczabinski@gmail.com>
2025-07-12 13:48:54 +0200

    Adjust api routes and their accessibility

--- weight-api/routes/api.php
+++ weight-api/routes/api.php
@@ -5,16 +5,16 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/weights', [WeightController::class, 'store']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {return $request->user();});
+    Route::get('/list', function () {
+        return App\Models\User::all();
+    });
 });
 
 
