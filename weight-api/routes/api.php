43fedde4d2dd4a96706ff9f9b1c74077582cdabc
adamusek-007 <adam.kuczabinski@gmail.com>
2025-07-12 13:20:42 +0200

    Delete middleware for login and register

--- weight-api/routes/api.php
+++ weight-api/routes/api.php
@@ -9,15 +9,17 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Route::post('weights', [WeightController::class, 'store']);
-
-Route::post('/login', [AuthController::class, 'login'])->middleware('auth:sanctum');
-Route::post('/register', [AuthController::class, 'register'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/weights', [WeightController::class, 'store']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
+
+
+
+// Route::post('weights', [WeightController::class, 'store']);
 // Route::post('register', [RegisterController::class, 'register'])->name('register');
 // Route::post('login', [RegisterController::class, 'login'])->name('login');
