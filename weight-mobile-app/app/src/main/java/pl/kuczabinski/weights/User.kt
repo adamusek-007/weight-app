45a07e483caa8017a26973e2d0b221f1fe533dbd
adamusek-007 <adam.kuczabinski@gmail.com>
2025-07-13 13:14:31 +0200

    Add constructors for User Class

--- weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/User.kt
+++ weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/User.kt
@@ -1,7 +1,18 @@
 package pl.kuczabinski.weights
 
-class User(
-    var name: String,
-    var email: String,
+class User {
+    var name: String? = null
+    var email: String
     var password: String
-)
+
+    constructor(email: String, password: String) {
+        this.email = email
+        this.password = password
+    }
+
+    constructor(name: String, email: String, password: String) {
+        this.name = name
+        this.email = email
+        this.password = password
+    }
+}
