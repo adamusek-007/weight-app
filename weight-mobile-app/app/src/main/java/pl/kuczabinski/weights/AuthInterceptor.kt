f3f4111c3e1ff1db06b139995d94c0cac42520d1
adamusek-007 <adam.kuczabinski@gmail.com>
2025-09-07 19:30:01 +0200

    Create AuthInterceptor

---
+++ weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/AuthInterceptor.kt
@@ -0,0 +1,24 @@
+package pl.kuczabinski.weights
+
+import android.content.Context
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private val context: Context) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val sharedPref =
+            context.getSharedPreferences("pl.kuczabinski.weights", Context.MODE_PRIVATE)
+        val token = sharedPref.getString("token", null)
+
+        val originalRequest = chain.request()
+        val builder = originalRequest.newBuilder()
+
+        if (token != null) {
+            builder.addHeader("Authorization", "Bearer $token")
+        }
+
+        builder.addHeader("Accept", "application/json")
+
+        return chain.proceed(builder.build())
+    }
+}
