48028211ded90415ffb38e203c0352f230f1a3be
adamusek-007 <adam.kuczabinski@gmail.com>
2025-09-08 15:16:27 +0200

    Add charts dependency

--- weight-mobile-app/app/build.gradle.kts
+++ weight-mobile-app/app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("com.github.PhillJay:MPAndroidChart:v.3.1.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation(libs.androidx.core.ktx)
