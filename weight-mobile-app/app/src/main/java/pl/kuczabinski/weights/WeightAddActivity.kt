523a6dfc480a0fec1c102413a3ccd735d588a658
adamusek-007 <adam.kuczabinski@gmail.com>
2025-09-12 18:16:38 +0200

    Move classes to separate package

--- weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/WeightAddActivity.kt
+++ weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/WeightAddActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import okhttp3.ResponseBody
+import pl.kuczabinski.weights.remote.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
