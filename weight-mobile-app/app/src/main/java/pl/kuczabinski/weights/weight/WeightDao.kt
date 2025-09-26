f0df9b50ccc999b83e75e431bd661b824b7ef298
adamusek-007 <adam.kuczabinski@gmail.com>
2025-09-26 07:59:26 +0200

    Update Dao to match naming

--- weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/weight/WeightDao.kt
+++ weight-mobile-app/app/src/main/java/pl/kuczabinski/weights/weight/WeightDao.kt
@@ -1,34 +1,24 @@
-package pl.kuczabinski.weights.db
+package pl.kuczabinski.weights.weight
 
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
-import pl.kuczabinski.weights.Weight
 
 @Dao
-
-
 interface WeightDao {
 
     @Insert
-    fun insert(weightEntry: WeightEntry): String
+    fun insert(weightEntry: DBEntry): Long
 
     @Update
-    fun update(weightEntry: WeightEntry)
+    fun update(weightEntry: DBEntry)
     @Delete
-    fun delete(id: String)
-
+    fun delete(weightEntry: DBEntry)
     @Query("SELECT * FROM weight_entries ORDER BY id ASC")
-    fun getAll(): List<WeightEntry>
+    fun getAll(): List<DBEntry>
 
-    @Query("SELECT * FROM weight_entries WHERE id = :id")
-    fun getById(id: Long)
-
-
-
-
-
-
+    @Query("SELECT * FROM weight_entries WHERE id = :id LIMIT 1")
+    fun getById(id: Long): DBEntry?
 }
